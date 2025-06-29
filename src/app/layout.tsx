ad3ac1b5b60b1dbf03586ec736e7cc14ab7f0e48
ncohen98 <cohenhadas00@gmail.com>
2025-06-30 00:39:18 +0100

    add layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,11 @@
+// src/app/layout.tsx
+import React from 'react';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
